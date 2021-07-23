6a1e9df9cf425acf2a8485d9b99fa9545c96d5c2
Mir Hussain <mir.hm110@gmail.com>
2021-07-23 11:37:17 +0600

    nav layout done

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,17 +1,35 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
+
+import logo from "../public/images/logo.png";
+
 const Navbar = () => {
   return (
     <nav>
+      <div className='logo'>
+        <div className='logo__image'>
+          <Image
+            src='/../public/images/logo.png'
+            alt='logo'
+            layout='fill'
+            objectFit='contain'
+          />
+        </div>
+        <h1 className='logo__text'>Henosis</h1>
+      </div>
       <ul>
         <li>
-          <Link href='/'>Home</Link>
+          <Link href='/features'>Features</Link>
         </li>
         <li>
-          <Link href='/'>Home</Link>
+          <Link href='/pricing'>Pricing</Link>
         </li>
         <li>
-          <Link href='/'>Home</Link>
+          <Link href='/signin'>Sign In</Link>
+        </li>
+        <li>
+          <Link href='/signup'>Sign Up</Link>
         </li>
       </ul>
     </nav>
