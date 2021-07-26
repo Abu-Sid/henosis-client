8a97b0cb0e7f388733bb656113e2e695eb5441b4
Mir Hussain <mir.hm110@gmail.com>
2021-07-26 21:03:15 +0600

    made some changes and added the images

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,21 @@
 import React from "react";
+import Image from "next/image";
+import dashboard from "../public/images/dashboard.svg";
+import purpleDots from "../public/images/purple-dots.svg";
+import grayDots from "../public/images/gray-dots.svg";
+import invitations from "../public/images/invitation.svg";
+import tasks from "../public/images/tasks.svg";
+import workspace from "../public/images/workspace.svg";
 
 const Home = () => {
   return (
-    <div>
-      <h1>This is home</h1>
-    </div>
+    <section className='home-section'>
+      <div className='intro-text'>
+        <h1>Get your work done. Together</h1>
+        <p>Project management and collaboration made easy.</p>
+      </div>
+      <div className='dashboard'></div>
+    </section>
   );
 };
 
