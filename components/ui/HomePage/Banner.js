419c3e892c984727fdca30bdc00a7bf1c711ff5b
Abu Siddique <asrafeee@yahoo.com>
2021-07-27 18:59:55 -0400

    banner feature section done

--- components/ui/HomePage/Banner.js
+++ components/ui/HomePage/Banner.js
@@ -5,11 +5,13 @@ import banner from '../../../public/images/banner.png';
 const Banner = () => {
     return (
         <div className='banner'>
-            <div className='banner__text'>
-                <h1>Get your work done. <br /> <span>Together.</span> </h1>
-                <h4>Project Management and Collaboration made easy by <span> Henosis</span>.</h4>
-                <button className='button-primary'>GET STARTED</button>
-            </div>
+            
+                <div className='banner__text'>
+                    <h1>Get your work done. <br /> <span>Together.</span> </h1>
+                    <h4>Project Management and Collaboration made <br /> easy by <span> Henosis</span>.</h4>
+                    <button className='button-primary'>GET STARTED</button>
+                </div>
+            
             <div className='banner__image'>
                 <Image src={banner} alt='banner' />
             </div>
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Footer from "../components/ui/Footer";
 import HomePage from "../components/ui/HomePage/HomePage";
 
 const Home = () => {
   return (
     <section className='home-section'>
       <HomePage/>
-      <Footer/>
     </section>
   );
 };
