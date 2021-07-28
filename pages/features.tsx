9cde67eefed0b38b657e3caa794e68cb34b73d93
Mir Hussain <mir.hm110@gmail.com>
2021-07-28 13:53:25 +0600

    working on features page intro

--- pages/features.tsx
+++ pages/features.tsx
@@ -5,6 +5,13 @@ import FeatureSubSection from "../components/ui/Feature/FeatureSubSection";
 const Features = () => {
   return (
     <section className='features-section'>
+      <div className='features-section__intro'>
+        <h1>Features for large scale development.</h1>
+        <h2>
+          Henosis Software is the project management tool for agile teams.
+        </h2>
+        <button>Get Started</button>
+      </div>
       <FeatureSubSection />
     </section>
   );
