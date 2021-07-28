75c05750314804b837e608065ce4dc4d8deb9622
Mir Hussain <mir.hm110@gmail.com>
2021-07-28 13:01:08 +0600

    semiclon issue solved

--- pages/features.tsx
+++ pages/features.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import Gear from "../components/Gear";
+import FeatureSubSection from "../components/ui/Feature/FeatureSubSection";
 
 const Features = () => {
-  return <section className='features-section'></section>;
+  return (
+    <section className='features-section'>
+      <FeatureSubSection />
+    </section>
+  );
 };
 
 export default Features;
