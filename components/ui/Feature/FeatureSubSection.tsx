5bac496c89e6153cf5adb0d84e32c22a06525d3e
Mir Hussain <mir.hm110@gmail.com>
2021-07-28 12:31:01 +0600

    transparent bg problem solved

---
+++ components/ui/Feature/FeatureSubSection.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FeatureSubSection = () => {
+  return (
+    <div>
+      <h1>This is a sub section</h1>
+    </div>
+  );
+};
+
+export default FeatureSubSection;
---
+++ pages/features.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Gear from "../components/Gear";
+
+const Features = () => {
+  return <section className='features-section'></section>;
+};
+
+export default Features;
