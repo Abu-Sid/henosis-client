d1295ec9eb197cd21ee57cd08066b9041ded7b59
Abu Siddique <asrafeee@yahoo.com>
2021-07-29 09:13:03 -0400

    dashboard section bug fixed

--- components/ui/HomePage/HomePage.js
+++ components/ui/HomePage/HomePage.js
@@ -3,7 +3,7 @@ import Footer from '../Footer';
 import ActionToCall from './ActionToCall';
 import Banner from './Banner';
 import BoardSection from './BoardSection';
-import DashboardSection from './Dashboardsection';
+import DashboardSection from './DashboardSection';
 import FeaturesSection from './FeaturesSection';
 
 const HomePage = () => {
