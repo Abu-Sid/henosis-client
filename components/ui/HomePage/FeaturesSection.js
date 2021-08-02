818dfcc1043ef9d1ca69ebb8e245083c383a9a36
Abu Siddique <asrafeee@yahoo.com>
2021-08-01 23:47:04 -0400

    homepage responsive done

--- components/ui/HomePage/FeaturesSection.js
+++ components/ui/HomePage/FeaturesSection.js
@@ -7,7 +7,7 @@ const FeaturesSection = () => {
         <div className='features'>
             <div className='features__text'>
                 <h1>It’s more than work. It’s a way of working <span>together</span> .</h1>
-                <h4>Start with a <span>Henosis </span> board. Customize and expand with more features as <br /> your teamwork grows. Manage projects, organize tasks, and build <br />  team spirit—all in one place.</h4>
+                <h4>Start with a <span>Henosis </span> board. Customize and expand with more features as your teamwork grows. Manage projects, organize tasks, and build team spirit—all in one place.</h4>
             </div>
             <div className='features__image'>
                 <Image src={board} alt='board' />
