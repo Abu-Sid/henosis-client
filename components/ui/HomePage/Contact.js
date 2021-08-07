197b2604997faeb29f17d298d055b7e885060da7
nrd141913 <nrd141913@.com>
2021-08-07 22:08:44 +0600

    contact form done

--- components/ui/HomePage/Contact.js
+++ components/ui/HomePage/Contact.js
@@ -1,10 +1,38 @@
 import React from "react";
-import Link from 'next/link'
+import emailjs from "emailjs-com";
 
 const Contact = () => {
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_73qb7cg",
+        "template_oznqcux",
+        e.target,
+        "user_yHYAoZsap8fcwMhDqGTPM"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
-    <div className="">
-      
+    <div className="contact-container">
+      <h2>Contact Us</h2>
+      <form className="contact-form" onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="name" required />
+        <label>Email</label>
+        <input type="email" name="email" required />
+        <label>Message</label>
+        <textarea name="message" cols="10" rows="8" required />
+        <input type="submit" value="Send" className="button" />
+      </form>
     </div>
   );
 };
