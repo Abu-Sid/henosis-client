7563c5f050dd0e8a8982a57bef6c9c187f3c2c62
Mir Hussain <mir.hm110@gmail.com>
2021-08-10 01:11:46 +0600

    board started

--- components/Workspace/WorkspacePages/Board.tsx
+++ components/Workspace/WorkspacePages/Board.tsx
@@ -10,12 +10,30 @@ const Board = () => {
   const { workspaceName } = workspace;
 
   return (
-    <div>
-      <h3>Board/{workspaceName}</h3>
-      <h1>
-        {workspaceName}/ <span></span>
-      </h1>
-    </div>
+    <section className='board-section'>
+      <div className='board-section__indicator'>
+        <p>
+          <span> Board </span> /{workspaceName}
+        </p>
+      </div>
+      <div className='board-section__header'>
+        <div className='board-section__objective'>
+          <h1>
+            <span>{workspaceName}</span> / Sprint 1 objective
+          </h1>
+          <ul>
+            <li>Getting started with project.</li>
+            <li>Setting up initials.</li>
+            <li>Designing the homepage.</li>
+          </ul>
+        </div>
+        <div className='board-section__actions'>
+          <p>Starts from 18 Jul, Ends in 25 Jul</p>
+          <button className='button-primary'>End Scrum</button>
+          <button>...</button>
+        </div>
+      </div>
+    </section>
   );
 };
 
