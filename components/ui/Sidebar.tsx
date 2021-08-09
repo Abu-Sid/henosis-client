1154ccbfddfbade0fd7e6cf8ff36fa86de04f06d
Mir Hussain <mir.hm110@gmail.com>
2021-08-09 11:23:18 +0600

    resolved logo problem

--- components/ui/Sidebar.tsx
+++ components/ui/Sidebar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import Logo from "../../public/images/logo.png";
+import Logo from "../../public/images/logo.svg";
 import Menu from "../../public/images/icons/menu.svg";
 import User from "../../public/images/icons/user.svg";
 import ClipBoard from "../../public/images/icons/clipboard-check.svg";
