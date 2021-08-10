00cb6a4b461643e7fa51eeca6ba4528e962170da
nrd141913 <nrd141913@.com>
2021-08-10 22:24:01 +0600

    admin part added

--- pages/dashboard/admins.js
+++ pages/dashboard/admins.js
@@ -1,13 +1,88 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import "react-responsive-modal/styles.css";
+import { Modal } from "react-responsive-modal";
+import { useForm } from "react-hook-form";
+import TableRow from "../../components/Dashboard/TableRow";
 import SideBar from "../../components/Dashboard/SideBar";
 
-const admins = () => {
-    return (
-        <div style={{marginTop:'60px'}}>
-            <SideBar />
-            <h1>dashboard/admins</h1>
+const Admins = () => {
+  const [adminInfo, setAdminInfo] = useState([]);
+  useEffect(() => {
+    fetch("https://intense-peak-24388.herokuapp.com/admin")
+      .then((res) => res.json())
+      .then((data) => setAdminInfo(data.data));
+  }, []);
+  const [open, setOpen] = useState(false);
+  const onOpenModal = () => setOpen(true);
+  const onCloseModal = () => setOpen(false);
+
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data, e) => {
+    if (data) {
+      const { name, email, role } = data;
+      // sent data to database
+      fetch("https://intense-peak-24388.herokuapp.com/admin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, name, role }),
+      });
+      e.target.reset();
+      setOpen(false);
+    }
+  };
+
+  return (
+    <section className="d-container">
+      <div className="d-row">
+        <div className="col-left">
+          <SideBar />
         </div>
-    );
+        <div className="col-right">
+          <div className="right-division">
+            <h2>Admins</h2>
+            <button className="admin-button" onClick={onOpenModal}>
+              Add New Admin
+            </button>
+            <Modal open={open} onClose={onCloseModal} center id="modal">
+              <div className="admin-info">
+                <h2>Admin Infomation</h2>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                  <label>Admin Name</label>
+                  <input {...register("name")} type="text" required />
+                  <label>Email</label>
+                  <input {...register("email")} type="email" />
+                  <label>Role</label>
+                  <select {...register("role")} type="text">
+                    <option value="admin">Admin</option>
+                    <option value="editor">Editor</option>
+                    <option value="other">other</option>
+                  </select>
+                  <input type="submit" value="Add Admin" className="button" />
+                </form>
+              </div>
+            </Modal>
+          </div>
+          <table>
+            <thead>
+              <tr>
+                <th>No.</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Role</th>
+              </tr>
+            </thead>
+            <tbody>
+              {adminInfo.map((info, index) => (
+                <TableRow key={info._id} info={info} index={index} />
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </section>
+  );
 };
 
-export default admins;
+export default Admins;
