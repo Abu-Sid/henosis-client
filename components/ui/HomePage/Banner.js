e40606109922e470b8de82cd84a96fed16629b46
Mir Hussain <mir.hm110@gmail.com>
2021-08-10 19:58:51 +0600

    animation adde to home page

--- components/ui/HomePage/Banner.js
+++ components/ui/HomePage/Banner.js
@@ -1,22 +1,36 @@
-import Image from 'next/image';
-import React from 'react';
-import banner from '../../../public/images/banner.png';
+import Image from "next/image";
+import React from "react";
+import banner from "../../../public/images/banner.png";
 
 const Banner = () => {
-    return (
-        <div className='banner'>
-            
-                <div className='banner__text'>
-                    <h1>Get your work done. <br /> <span>Together.</span> </h1>
-                    <h4>Project Management and Collaboration made easy by <span> Henosis</span>.</h4>
-                    <button className='button-primary'>GET STARTED</button>
-                </div>
-            
-            <div className='banner__image'>
-                <Image src={banner} alt='banner' />
-            </div>
+  return (
+    <div className='banner'>
+      <div className='banner__text'>
+        <div data-aos='fade-down' data-aos-duration='1000'>
+          <h1>
+            Get your work done. <br /> <span>Together.</span>{" "}
+          </h1>
+          <h4>
+            Project Management and Collaboration made easy by{" "}
+            <span> Henosis</span>.
+          </h4>
         </div>
-    );
+
+        <button
+          data-aos='fade-right'
+          data-aos-duration='1000'
+          data-aos-delay='1000'
+          className='button-primary'
+        >
+          GET STARTED
+        </button>
+      </div>
+
+      <div className='banner__image'>
+        <Image src={banner} alt='banner' />
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
--- components/ui/HomePage/BoardSection.js
+++ components/ui/HomePage/BoardSection.js
@@ -1,19 +1,27 @@
-import Image from 'next/image';
-import React from 'react';
-import chat from '../../../public/images/chat.png';
+import Image from "next/image";
+import React from "react";
+import chat from "../../../public/images/chat.png";
 const BoardSection = () => {
-    return (
-        <div className='chat'>
-            <div className='chat__image'>
-                <Image src={chat} alt='chat' />
-            </div>
-            <div className='chat__text'>
-                <h4>Group Chat</h4>
-                <h2>The board is just the <span>beginning</span> .</h2> <br />
-                <p>You can <span>chat with other developers</span>  in your group, while working on a feature or solving a problem.</p>
-            </div>
-        </div>
-    );
+  return (
+    <div className='chat'>
+      <div className='chat__image'>
+        <Image src={chat} alt='chat' />
+      </div>
+      <div className='chat__text'>
+        <h4 data-aos='fade-left' data-aos-delay='100'>
+          Group Chat
+        </h4>
+        <h2 data-aos='fade-left' data-aos-delay='200'>
+          The board is just the <span>beginning</span>
+        </h2>
+        <br />
+        <p data-aos='fade-left' data-aos-delay='300'>
+          You can <span>chat with other developers</span> in your group, while
+          working on a feature or solving a problem.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default BoardSection;
