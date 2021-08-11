37dd3b6c986ef3c2e612bee324089f632bb4bfe8
saifulislamsojib <saifulislam.sojib56@gmail.com>
2021-08-11 13:29:33 +0600

    sprint-redux-finished

---
+++ redux/actions/sprintActions/actionInterface.ts
@@ -0,0 +1,36 @@
+import { ActionType } from "./actionTypes";
+
+export interface ISprint {
+  status: string[];
+  tasks: ITask[];
+  sprintName: string;
+  startDate: Date;
+  endDate: Date;
+  workspaceId: string;
+  goals: string[];
+  _id?: string;
+}
+
+export interface ITask {
+  _id?: string;
+  taskName: string;
+  currentStatus: string;
+  dueDate: Date;
+  assignedMember: string[];
+}
+
+interface ISprintAction {
+  type: ActionType.SEND_CURRENT_SPRINT | ActionType.CREATED_SPRINT;
+  payload: ISprint;
+}
+
+interface ITaskAction {
+  type: ActionType.ADDED_TASK;
+  payload: ITask[];
+}
+
+interface ISprintEmpty {
+  type: ActionType.SET_EMPTY_SPRINT;
+}
+
+export type IAction = ISprintAction | ITaskAction | ISprintEmpty;
--- redux/reducers/index.ts
+++ redux/reducers/index.ts
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
 import routerStateReducer, { RouterState } from "./routerStateReducer";
+import sprintReducer, { ISprintState } from "./sprintReducer";
 import userReducer, { UserState } from "./userReducer";
 import workspaceReducer, { IWorkspaceState } from "./workspaceReducer";
 
@@ -7,12 +8,14 @@ export interface RootState {
   userReducer: UserState;
   routerState: RouterState;
   workspaceReducer: IWorkspaceState;
+  sprintReducer: ISprintState;
 }
 
 const rootReducer = combineReducers({
   userReducer,
   routerState: routerStateReducer,
   workspaceReducer,
+  sprintReducer,
 });
 
 export default rootReducer;
