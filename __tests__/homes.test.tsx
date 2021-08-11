b8f5378859994e1d845f16b9bb4131f7e2fce86a
Abu Siddique <asrafeee@yahoo.com>
2021-08-11 02:19:10 -0400

    testing dependency installed & 2 independent test done

--- __tests__/homes.test.tsx
+++ __tests__/homes.test.tsx
@@ -4,9 +4,9 @@ import Homes from "../components/ui/Homes";
 test("renders a message", () => {
   const { container, getByText } = render(<Homes />);
   expect(getByText("Hello World")).toBeInTheDocument();
-  //   expect(container.firstChild).toMatchInlineSnapshot(`
-  //     <p>
-  //       Hello World
-  //     </p>
-  //   `);
+  expect(container.firstChild).toMatchInlineSnapshot(`
+      <p>
+        Hello World
+      </p>
+    `);
 });
