82813dfde601f965e748b8ed940e9c4da5bb46e3
Mir Hussain <mir.hm110@gmail.com>
2021-08-15 19:47:59 +0600

    settingRoute added

---
+++ pages/settings/[...paths].tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SettingsSidebar from "../../components/Settings/SettingsSidebar";
+
+const SettingsPage = () => {
+  return (
+    <div>
+      <SettingsSidebar />
+    </div>
+  );
+};
+
+export default SettingsPage;
--- pages/settings/index.tsx
+++ pages/settings/index.tsx
@@ -4,7 +4,11 @@ import SettingsSidebar from "../../components/Settings/SettingsSidebar";
 import { RootState } from "../../redux/reducers";
 const Settings = () => {
   const { user } = useSelector((state: RootState) => state.userReducer);
-  return <SettingsSidebar />;
+  return (
+    <div>
+      <h1>Settings</h1>
+    </div>
+  );
 };
 
 export default Settings;
