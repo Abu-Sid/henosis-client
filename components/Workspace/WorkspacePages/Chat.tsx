e891824e56c6144b51fd677b7a043ab6ecf515c2
Mir Hussain <mir.hm110@gmail.com>
2021-08-16 02:14:44 +0600

    chat portal basic layout done

--- components/Workspace/WorkspacePages/Chat.tsx
+++ components/Workspace/WorkspacePages/Chat.tsx
@@ -4,9 +4,12 @@ import LoadingAnimation from "../../ui/Animation/LoadingAnimation";
 const Chat = () => {
   return (
     <section className='chat-portal'>
-      <div className='channel'></div>
-      <div className='body'></div>
-      <div className='members'></div>
+      <div className='chat-portal__header'></div>
+      <div className='chat-portal__body'>
+        <div className='channel'></div>
+        <div className='chat-body'></div>
+        <div className='members'></div>
+      </div>
     </section>
   );
 };
