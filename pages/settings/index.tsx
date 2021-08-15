fea1d527bda120ec3641aa555170581220579632
Mir Hussain <mir.hm110@gmail.com>
2021-08-15 14:36:19 +0600

    settings sidebar icon added

--- pages/settings/index.tsx
+++ pages/settings/index.tsx
@@ -2,10 +2,24 @@ import React from "react";
 import SidebarContainer from "../../components/ui/Sidebar/SidebarContainer";
 import SidebarItem from "../../components/ui/Sidebar/SidebarItem";
 import Logo from "../../public/images/logo.svg";
+import Profile from "../../public/images/icons/emoji-happy.svg";
+import Account from "../../public/images/icons/user-circle.svg";
+import Billing from "../../public/images/icons/credit-card.svg";
+import Customization from "../../public/images/icons/settings.svg";
+import Logout from "../../public/images/icons/logout.svg";
 const Settings = () => {
   return (
     <SidebarContainer>
       <SidebarItem icon={Logo} className='sidebar__logo' href='/' />
+      <SidebarItem icon={Profile} pathName='/settings' href='profile' />
+      <SidebarItem icon={Account} pathName='/settings' href='account' />
+      <SidebarItem
+        icon={Customization}
+        pathName='/settings'
+        href='/customization'
+      />
+      <SidebarItem icon={Billing} pathName='/settings' href='billing' />
+      <SidebarItem icon={Logout} className='sidebar__logout' />
     </SidebarContainer>
   );
 };
