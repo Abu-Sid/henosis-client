6020065bfec089d537f8cd061a737e049b78caf6
Mir Hussain <mir.hm110@gmail.com>
2021-08-16 20:03:42 +0600

    channel user option done

--- components/Workspace/WorkspacePages/Chat/Chat.tsx
+++ components/Workspace/WorkspacePages/Chat/Chat.tsx
@@ -1,6 +1,10 @@
 import React from "react";
-import LoadingAnimation from "../../../ui/Animation/LoadingAnimation";
+import Image from "next/image";
 import ChatHeader from "./ChatHeader";
+import User from "../../../../public/images/icons/user-pp.svg";
+import Gear from "../../../../public/images/icons/cog.svg";
+import Mic from "../../../../public/images/icons/microphone.svg";
+import Speaker from "../../../../public/images/icons/volume-up.svg";
 
 const Chat = () => {
   return (
@@ -14,7 +18,25 @@ const Chat = () => {
               <button>+</button>
             </div>
           </div>
-          <div className='channel__options'></div>
+          <div className='channel__options'>
+            <div className='options-user-pp'>
+              <Image
+                src={User}
+                alt='user profile picture'
+                height={40}
+                width={40}
+              />
+            </div>
+            <div className='options-icon'>
+              <Image src={Speaker} alt='Speaker Icon' height={24} width={24} />
+            </div>
+            <div className='options-icon'>
+              <Image src={Mic} alt='Microphone Icon' height={24} width={24} />
+            </div>
+            <div className='options-icon'>
+              <Image src={Gear} alt='Gear Icon' height={24} width={24} />
+            </div>
+          </div>
         </div>
         <div className='chat-body'></div>
         <div className='members'></div>
