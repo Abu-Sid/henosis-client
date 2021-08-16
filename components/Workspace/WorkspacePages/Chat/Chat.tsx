4f3c1a92dc90e470735b6e54d5d8f13a6548c0d4
Mir Hussain <mir.hm110@gmail.com>
2021-08-16 21:16:05 +0600

    chatportal channel section done

--- components/Workspace/WorkspacePages/Chat/Chat.tsx
+++ components/Workspace/WorkspacePages/Chat/Chat.tsx
@@ -1,43 +1,13 @@
 import React from "react";
-import Image from "next/image";
+import ChatChannel from "./ChatChannel";
 import ChatHeader from "./ChatHeader";
-import User from "../../../../public/images/icons/user-pp.svg";
-import Gear from "../../../../public/images/icons/cog.svg";
-import Mic from "../../../../public/images/icons/microphone.svg";
-import Speaker from "../../../../public/images/icons/volume-up.svg";
 
 const Chat = () => {
   return (
     <section className='chat-portal'>
       <ChatHeader />
       <div className='chat-portal__body'>
-        <div className='channel'>
-          <div className='channel__text'>
-            <div className='channel__text-header'>
-              <h1>Text Channels</h1>
-              <button>+</button>
-            </div>
-          </div>
-          <div className='channel__options'>
-            <div className='options-user-pp'>
-              <Image
-                src={User}
-                alt='user profile picture'
-                height={40}
-                width={40}
-              />
-            </div>
-            <div className='options-icon'>
-              <Image src={Speaker} alt='Speaker Icon' height={24} width={24} />
-            </div>
-            <div className='options-icon'>
-              <Image src={Mic} alt='Microphone Icon' height={24} width={24} />
-            </div>
-            <div className='options-icon'>
-              <Image src={Gear} alt='Gear Icon' height={24} width={24} />
-            </div>
-          </div>
-        </div>
+        <ChatChannel />
         <div className='chat-body'></div>
         <div className='members'></div>
       </div>
