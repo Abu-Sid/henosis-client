a6f8b0b4475f52765a21b83a982247c80ee8aa8d
Mir Hussain <mir.hm110@gmail.com>
2021-08-16 16:10:19 +0600

    working on channgle section

--- components/Workspace/WorkspacePages/Chat/Chat.tsx
+++ components/Workspace/WorkspacePages/Chat/Chat.tsx
@@ -7,7 +7,10 @@ const Chat = () => {
     <section className='chat-portal'>
       <ChatHeader />
       <div className='chat-portal__body'>
-        <div className='channel'></div>
+        <div className='channel'>
+          <div className='channel__text'></div>
+          <div className='channel__options'></div>
+        </div>
         <div className='chat-body'></div>
         <div className='members'></div>
       </div>
