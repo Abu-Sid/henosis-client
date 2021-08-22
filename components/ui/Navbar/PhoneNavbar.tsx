fa3e07b49126d419e3f66336fbd1b857b08c800d
Mir Hussain <mir.hm110@gmail.com>
2021-08-22 18:04:10 +0600

    admin option added to phone-navbar

--- components/ui/Navbar/PhoneNavbar.tsx
+++ components/ui/Navbar/PhoneNavbar.tsx
@@ -1,19 +1,20 @@
 import { IconProp } from "@fortawesome/fontawesome-svg-core";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
+import { useContext } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../../../auth/authManager";
 import { authUserLogout } from "../../../redux/actions/userActions";
 import { RootState } from "../../../redux/reducers";
 import { DropdownItem, DropdownMenu } from "./DropDown";
 import Nav from "./Nav";
+import { UserContext } from "./Navbar";
 import NavItem from "./NavItem";
 
 const PhoneNavbar = () => {
-  const dispatch = useDispatch();
+  const { username, admin } = useContext(UserContext);
 
-  const { user } = useSelector((state: RootState) => state.userReducer);
-  const username = user?.name;
+  const dispatch = useDispatch();
 
   const handleLogout = async () => {
     try {
@@ -30,6 +31,7 @@ const PhoneNavbar = () => {
         <DropdownMenu width={250}>
           <DropdownItem href='/features'>Features</DropdownItem>
           <DropdownItem href='/pricing'>Pricing</DropdownItem>
+          {admin && <NavItem name='Admin' href='/dashboard/admins' />}
           {!username && <DropdownItem href='/signup'>Sign Up</DropdownItem>}
           {!username && <DropdownItem href='/signin'>Sign In</DropdownItem>}
           {username && (
