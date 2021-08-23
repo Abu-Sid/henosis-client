0f21fe5ce435d9b033c747147a59bf4f1ca4bde6
Mir Hussain <mir.hm110@gmail.com>
2021-08-23 23:59:23 +0600

    preloader initial path and fill animation added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,20 +7,44 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
 import ScrollToTop from "react-scroll-to-top";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import Preloader from "../components/ui/Animation/Preloader";
 
 function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
     AOS.init();
   }, []);
+
+  // const pageIsReady = useRouter().isReady;
+  // console.log(router.isReady);
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  });
+
   return (
-    <Provider store={store}>
-      <Layout>
-        <Component {...pageProps} />
-        <div className='scroll-to-top'>
-          <ScrollToTop smooth color='white' className='scroll-to-top__button' />
-        </div>
-      </Layout>
-    </Provider>
+    <>
+      {loading && <Preloader />}
+      {!loading && (
+        <Provider store={store}>
+          <Layout>
+            <Component {...pageProps} />
+            <div className='scroll-to-top'>
+              <ScrollToTop
+                smooth
+                color='white'
+                className='scroll-to-top__button'
+              />
+            </div>
+          </Layout>
+        </Provider>
+      )}
+    </>
   );
 }
 
