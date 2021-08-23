2d64747d6e6c2327caac5f2dbb04ceef03fa21e3
Mir Hussain <mir.hm110@gmail.com>
2021-08-24 01:06:30 +0600

    some minor backu

--- components/ui/Animation/Preloader.tsx
+++ components/ui/Animation/Preloader.tsx
@@ -61,6 +61,7 @@ const Preloader = () => {
           <motion.path
             initial='hidden'
             animate='animationTwo'
+            // animate='animationOne'
             variants={pathVariant}
             id='top'
             d='M224.771 1.5H254.095L225.684 24.9943L224.286 26.1503L225.684 27.3062L248.834 46.45L249.79 47.2406L250.746 46.4499L305.092 1.5H334.425L249.79 71.4904L194.963 26.1503L224.771 1.5Z'
@@ -70,6 +71,7 @@ const Preloader = () => {
           <motion.path
             initial='hidden'
             animate='animationThree'
+            // animate='animationOne'
             variants={pathVariant}
             id='bottom'
             d='M194.152 117.5H164.819L193.221 94.0133L194.619 92.8574L193.221 91.7014L170.08 72.5649L169.124 71.7745L168.168 72.5648L113.822 117.5H84.4979L169.124 47.5173L223.951 92.8574L194.152 117.5Z'
@@ -79,6 +81,7 @@ const Preloader = () => {
           <motion.path
             initial='hidden'
             animate='animationFour'
+            // animate='animationOne'
             variants={pathVariant}
             id='left'
             d='M385.722 1.5H415.064L274.782 117.501H245.458L385.722 1.5Z'
