6183bbdbadcb8afd48088952db6a2911962925ba
saifulislamsojib <saifulislam.sojib56@gmail.com>
2021-08-24 20:45:14 +0600

    add-channel

--- components/Workspace/WorkspacePages/Chat/Chat.tsx
+++ components/Workspace/WorkspacePages/Chat/Chat.tsx
@@ -1,10 +1,8 @@
-import Image from "next/image";
+import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 import { useSelector } from "react-redux";
 import useSocket from "../../../../hooks/useSocket";
-import Green from "../../../../public/images/icons/green.svg";
-import Orange from "../../../../public/images/icons/orange.svg";
-import Pink from "../../../../public/images/icons/pink.svg";
 import Purple from "../../../../public/images/icons/purple.svg";
 import { RootState } from "../../../../redux/reducers";
 import ChatBody from "./ChatBody";
@@ -18,18 +16,49 @@ export interface IChannel {
   users: string[];
 }
 
-const Chat = () => {
-  const socket = useSocket("/chat", "https://henosis-server-bd.herokuapp.com");
+interface IActive {
+  id?: string;
+  userId: string;
+  workspaceId: string;
+}
 
-  const { _id } = useSelector(
+const Chat = () => {
+  const { email } = useSelector((state: RootState) => state.userReducer.user);
+
+  const { _id, members } = useSelector(
     (state: RootState) => state.workspaceReducer.workspace
   );
 
+  const router = useRouter();
+
   const [channels, setChannels] = useState<IChannel[]>([]);
+
+  const [actives, setActives] = useState<IActive[]>([]);
+
+  const socket = useSocket("/chat", "http://localhost:5000");
+
+  const offline = members.filter(
+    (member) =>
+      !actives.map((activeMember) => activeMember.userId).includes(member._id)
+  );
 
   useEffect(() => {
     if (socket !== null) {
-      socket.emit("join-workspace", _id);
+      socket.on("change-activeList", (activeList: IActive[]) => {
+        setActives(activeList);
+      });
+    }
+  }, [socket]);
+
+  useEffect(() => {
+    const currentUser = members.find((member) => member.email === email);
+    if (socket !== null) {
+      socket.emit("join-workspace", { _id, userId: currentUser._id });
+    }
+  }, [socket, _id, email, members]);
+
+  useEffect(() => {
+    if (socket !== null) {
       socket.emit(
         "workspace-channels",
         _id,
@@ -44,34 +73,54 @@ const Chat = () => {
     }
   }, [socket, _id]);
 
+  useEffect(() => {
+    if (socket !== null) {
+      socket.on("created-channel", (channel: IChannel) => {
+        setChannels((preChannels) => [...preChannels, channel]);
+      });
+    }
+  }, [socket]);
+
+  const handleAddChannel = (data: IChannel) => {
+    const loadingId = toast.loading("loading...");
+    socket.emit("create-channel", data, (id: string) => {
+      toast.dismiss(loadingId);
+      toast.success("Channel Created Successfully!");
+      router.replace(`/workspaces/${_id}/chat/${id}`);
+    });
+  };
+
   return (
     <section className="chat-portal">
-      <ChatHeader />
+      <ChatHeader channels={channels} />
       <div className="chat-portal__body">
-        <ChatChannel channels={channels} />
-        <ChatBody channel={channels[0]} socket={socket} />
+        <ChatChannel channels={channels} handleAddChannel={handleAddChannel} />
+        <ChatBody channels={channels} socket={socket} />
         <div className="members">
           <div className="members__online">
-            <h3>Online (2)</h3>
-            <div className="chat-member">
-              <Image width={30} src={Purple} alt="profile picture" />
-              <p>Mir Hussain</p>
-            </div>
-            <div className="chat-member">
-              <Image width={30} src={Green} alt="profile picture" />
-              <p>Saiful Islam Sojib</p>
-            </div>
+            <h3>Online ({actives.length})</h3>
+            {actives.map((active) => {
+              const { id, userId } = active;
+              const activeUser = members.find(
+                (member) => member._id === userId
+              );
+              const { photo, name } = activeUser;
+              return (
+                <div key={id} className="chat-member">
+                  <img src={photo || Purple.src} alt="profile picture" />
+                  <p>{name}</p>
+                </div>
+              );
+            })}
           </div>
           <div className="members__offline">
-            <h3>Offline (2)</h3>
-            <div className="chat-member">
-              <Image width={30} src={Pink} alt="profile picture" />
-              <p>Abu Siddique</p>
-            </div>
-            <div className="chat-member">
-              <Image width={30} src={Orange} alt="profile picture" />
-              <p>Md Naimur</p>
-            </div>
+            <h3>Offline ({offline.length})</h3>
+            {offline.map((member) => (
+              <div key={member._id} className="chat-member">
+                <img src={member.photo || Purple.src} alt="profile picture" />
+                <p>{member.name}</p>
+              </div>
+            ))}
           </div>
         </div>
       </div>
