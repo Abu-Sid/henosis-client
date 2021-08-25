417ace35e9e15379ff6b5942472583891151041e
Mir Hussain <mir.hm110@gmail.com>
2021-08-25 20:39:36 +0600

    faq animation done

--- components/ui/HomePage/FAQ.tsx
+++ components/ui/HomePage/FAQ.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence, useAnimation } from "framer-motion";
 import FAQBanner from "../../../public/images/banner.png";
 import { useState } from "react";
 import FAQData from "../../../data/FAQData.json";
@@ -9,6 +9,8 @@ const FAQ = () => {
   const [selectedId, setSelectedId] = useState(null);
   const data = FAQData.FAQ;
   const answer = data?.find((data) => data.id === selectedId)?.answer;
+
+  const control = useAnimation();
   // const containerVariant = {
   //   hidden: { opacity: 0, y: -100 },
   //   visible: {
@@ -56,27 +58,24 @@ const FAQ = () => {
                     key={data.id}
                     onClick={() => setSelectedId(data.id)}
                   >
-                    <h2>{data.question}</h2>
-                    <motion.div
-                      initial={{ opacity: 0 }}
-                      animate={{ opacity: 1 }}
-                      exit={{ opacity: 0 }}
-                      className='down-indicator'
-                    >
-                      <svg
-                        width='20'
-                        height='10'
-                        viewBox='0 0 116 46'
-                        fill='none'
-                        xmlns='http://www.w3.org/2000/svg'
-                      >
-                        <path
-                          d='M8 8L58 36.4672L108 8'
-                          stroke='#171E3C'
-                          strokeWidth='15'
-                          strokeLinecap='round'
-                        />
-                      </svg>
+                    <motion.div>
+                      <p>{data.question}</p>
+                      <div className='down-indicator'>
+                        <svg
+                          width='20'
+                          height='10'
+                          viewBox='0 0 116 46'
+                          fill='none'
+                          xmlns='http://www.w3.org/2000/svg'
+                        >
+                          <path
+                            d='M8 8L58 36.4672L108 8'
+                            stroke='#171E3C'
+                            strokeWidth='10'
+                            strokeLinecap='round'
+                          />
+                        </svg>
+                      </div>
                     </motion.div>
                   </motion.div>
                 ))}
@@ -95,7 +94,49 @@ const FAQ = () => {
                 }}
                 variants={popupVariant}
               >
-                <button onClick={() => setSelectedId(null)}>Close</button>
+                <motion.button
+                  onHoverStart={() => {
+                    control.start({
+                      width: 25,
+                      height: 25,
+                    });
+                  }}
+                  onHoverEnd={() => {
+                    control.start({
+                      width: 20,
+                      height: 20,
+                    });
+                  }}
+                  onTapStart={() => {
+                    control.start({
+                      width: 20,
+                      height: 20,
+                    });
+                  }}
+                  onClick={() => setSelectedId(null)}
+                >
+                  <motion.svg
+                    initial={{ width: 20, height: 20 }}
+                    animate={control}
+                    viewBox='0 0 130 130'
+                    fill='none'
+                    xmlns='http://www.w3.org/2000/svg'
+                  >
+                    <circle cx='65' cy='65' r='65' fill='#E6E8EA' />
+                    <path
+                      d='M29.1445 30.1521L100.856 99.8481'
+                      stroke='#171E3C'
+                      strokeWidth='15'
+                      strokeLinecap='round'
+                    />
+                    <path
+                      d='M99.8481 29.1445L30.1522 100.856'
+                      stroke='#171E3C'
+                      strokeWidth='15'
+                      strokeLinecap='round'
+                    />
+                  </motion.svg>
+                </motion.button>
                 <p>{answer}</p>
               </motion.div>
             )}
