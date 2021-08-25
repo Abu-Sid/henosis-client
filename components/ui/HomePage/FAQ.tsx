8e760c7de1b65d21bdb724ba48b7e0f2d9daf30d
Mir Hussain <mir.hm110@gmail.com>
2021-08-25 14:47:41 +0600

    animation planning done

--- components/ui/HomePage/FAQ.tsx
+++ components/ui/HomePage/FAQ.tsx
@@ -9,19 +9,22 @@ const FAQ = () => {
   const [selectedId, setSelectedId] = useState(null);
   const data = FAQData.FAQ;
   const answer = data?.find((data) => data.id === selectedId)?.answer;
+  const containerVariant = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.2,
+      },
+    },
+  };
   const cardVariant = {
-    hidden: { opacity: 1 },
-    visible: { opacity: 0.5 },
+    hidden: { opacity: 0, y: -100 },
+    visible: { opacity: 1, y: 0 },
   };
   const popupVariant = {
     hidden: { opacity: 0, y: -100 },
-    visible: {
-      opacity: 1,
-      y: 0,
-      transition: {
-        type: "spring",
-      },
-    },
+    visible: { opacity: 1, y: 0 },
   };
 
   return (
@@ -30,18 +33,31 @@ const FAQ = () => {
         <Image src={FAQBanner} alt='banner' />
       </div>
       <div className='faq__cards-container'>
-        <div className='faq-card'>
-          {selectedId === null && (
-            <>
-              {data.map((data) => (
-                <motion.div className='faq-card__question' key={data.id}>
-                  <motion.div onClick={() => setSelectedId(data.id)}>
+        <motion.div
+          initial='hidden'
+          animate='visible'
+          variants={containerVariant}
+          className='faq-card'
+        >
+          <AnimatePresence>
+            {selectedId === null && (
+              <>
+                {data.map((data) => (
+                  <motion.div
+                    className='faq-card__question'
+                    initial='hidden'
+                    animate='visible'
+                    variants={cardVariant}
+                    exit={{ opacity: 0, y: -100 }}
+                    key={data.id}
+                    onClick={() => setSelectedId(data.id)}
+                  >
                     <h2>{data.question}</h2>
                   </motion.div>
-                </motion.div>
-              ))}
-            </>
-          )}
+                ))}
+              </>
+            )}
+          </AnimatePresence>
           <AnimatePresence>
             {selectedId && (
               <motion.div
@@ -51,9 +67,6 @@ const FAQ = () => {
                 exit={{
                   opacity: 0,
                   y: -100,
-                  transition: {
-                    duration: 0.5,
-                  },
                 }}
                 variants={popupVariant}
               >
@@ -62,7 +75,7 @@ const FAQ = () => {
               </motion.div>
             )}
           </AnimatePresence>
-        </div>
+        </motion.div>
       </div>
     </section>
   );
