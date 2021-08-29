1a235d6c5bc4440444acfa446239b7d4a51d41da
Mir Hussain <mir.hm110@gmail.com>
2021-08-30 01:46:21 +0600

    phone sidebar basic logic done

--- components/ui/Sidebar/SidebarContainer.tsx
+++ components/ui/Sidebar/SidebarContainer.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { motion, useAnimation } from "framer-motion";
 
 interface ISidebarContainer {
   device: string;
@@ -9,16 +10,32 @@ const SidebarContainer: React.FC<ISidebarContainer> = ({
   children,
   device,
 }) => {
+  const [hide, setHide] = useState(false);
+  const control = useAnimation();
   return (
-    <div
-      className={
-        device === "desktop" ? "sidebar-container" : "phone-sidebar-container"
-      }
-    >
-      <div className={device === "desktop" ? "sidebar" : "phone-sidebar"}>
-        <ul>{children}</ul>
-      </div>
-    </div>
+    <>
+      {hide && (
+        <button className='clickme2' onClick={() => setHide(false)}>
+          Click me sucker
+        </button>
+      )}
+      {!hide && (
+        <div
+          className={
+            device === "desktop"
+              ? "sidebar-container"
+              : "phone-sidebar-container"
+          }
+        >
+          <div className={device === "desktop" ? "sidebar" : "phone-sidebar"}>
+            <ul>{children}</ul>
+          </div>
+          <button className='clickme' onClick={() => setHide(true)}>
+            Click me
+          </button>
+        </div>
+      )}
+    </>
   );
 };
 
