2e8571bb4675159fc1a9ffbe74c5902c2fe27b74
Mir Hussain <mir.hm110@gmail.com>
2021-08-29 15:06:58 +0600

    working on subtask card

--- components/Workspace/WorkspacePages/PersonalDashboard/SubTask.tsx
+++ components/Workspace/WorkspacePages/PersonalDashboard/SubTask.tsx
@@ -4,6 +4,10 @@ interface ISubTaskHeader {
   handleAddSubTask: () => void;
 }
 
+// interface ISubTaskCard {
+
+// }
+
 const SubTask: React.FC = () => {
   const handleAddSubTask = () => {
     console.log("Add sub task clicked");
@@ -12,6 +16,7 @@ const SubTask: React.FC = () => {
   return (
     <>
       <SubTaskHeader handleAddSubTask={handleAddSubTask} />
+      <SubTaskCard />
     </>
   );
 };
@@ -21,6 +26,31 @@ const SubTaskHeader: React.FC<ISubTaskHeader> = ({ handleAddSubTask }) => {
     <div className='personal-dashboard__sub-task__header'>
       <h1>Add sub-task</h1>
       <div className='sub-task-add-button' onClick={handleAddSubTask}>
+        <svg
+          width='50'
+          height='50'
+          viewBox='0 0 50 50'
+          fill='none'
+          xmlns='http://www.w3.org/2000/svg'
+        >
+          <path
+            d='M25 18.75V25M25 25V31.25M25 25H31.25M25 25H18.75M43.75 25C43.75 35.3553 35.3553 43.75 25 43.75C14.6447 43.75 6.25 35.3553 6.25 25C6.25 14.6447 14.6447 6.25 25 6.25C35.3553 6.25 43.75 14.6447 43.75 25Z'
+            stroke='#4A4FFF'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+          />
+        </svg>
+      </div>
+    </div>
+  );
+};
+
+const SubTaskCard: React.FC = () => {
+  return (
+    <div className='personal-dashboard__sub-task__header'>
+      <h1>Add sub-task</h1>
+      <div className='sub-task-add-button'>
         <svg
           width='50'
           height='50'
