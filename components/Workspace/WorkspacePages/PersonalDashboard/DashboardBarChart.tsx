b5666d33e4809017594614819f8e179d3ccfec87
Mir Hussain <mir.hm110@gmail.com>
2021-08-30 21:14:48 +0600

    dashboard basic layout responsive

--- components/Workspace/WorkspacePages/PersonalDashboard/DashboardBarChart.tsx
+++ components/Workspace/WorkspacePages/PersonalDashboard/DashboardBarChart.tsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Bar } from "react-chartjs-2";
 
 const DashboardBarChart: React.FC = () => {
+  const [screenSize, setScreenSize] = useState(null);
+  useEffect(() => {
+    const currentScreenSize = window.innerWidth;
+    setScreenSize(currentScreenSize);
+  }, []);
+
+  const height = screenSize < 600 ? 200 : 600;
   return (
     <>
       <Bar
-        height={600}
+        height={height}
         width={350}
         data={{
           labels: ["Sat", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri"],
