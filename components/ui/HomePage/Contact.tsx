5d00e378e68ce6efac96365fff3e00755d1227e1
Abu Siddique <asrafeee@yahoo.com>
2021-08-30 12:18:12 -0400

    contact image added

--- components/ui/HomePage/Contact.tsx
+++ components/ui/HomePage/Contact.tsx
@@ -1,6 +1,7 @@
 import emailjs from "emailjs-com";
 import React from "react";
 import toast from "react-hot-toast";
+import contact from "../../../public/images/contact_us.png";
 
 const Contact = () => {
   const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
@@ -30,21 +31,26 @@ const Contact = () => {
   };
 
   return (
-    <div
-      data-testid="contact-header"
-      data-aos="zoom-in-up"
-      className="contact-container"
-    >
-      <h2>Contact Us</h2>
-      <form className="contact-form" onSubmit={sendEmail}>
-        <label>Name</label>
-        <input type="text" name="name" required />
-        <label>Email</label>
-        <input type="email" name="email" required />
-        <label>Message</label>
-        <textarea name="message" cols={10} rows={8} required />
-        <input type="submit" value="Send" className="button" />
-      </form>
+    <div>
+      <div>
+        {/* <Image src={} alt="contact us" /> */}
+        <img src={contact.src} alt="contact us" />
+      </div>
+      <div
+        data-testid="contact-header"
+        data-aos="zoom-in-up"
+        className="contact-container"
+      >
+        <h2>Contact Us</h2>
+        <form className="contact-form" onSubmit={sendEmail}>
+          <label>Name</label>
+          <input type="text" name="name" required />
+          <label>Email</label>
+          <input type="email" name="email" required /> <label>Message</label>
+          <textarea name="message" cols={10} rows={8} required />
+          <input type="submit" value="Send" className="button" />
+        </form>
+      </div>
     </div>
   );
 };
