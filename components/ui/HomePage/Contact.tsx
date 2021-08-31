52133819bb4f69e43f79a0e97f99f03e1fd14f32
Abu Siddique <asrafeee@yahoo.com>
2021-08-31 01:42:59 -0400

    contact us and faq responsive done

--- components/ui/HomePage/Contact.tsx
+++ components/ui/HomePage/Contact.tsx
@@ -2,6 +2,7 @@ import emailjs from "emailjs-com";
 import React from "react";
 import toast from "react-hot-toast";
 import contact from "../../../public/images/contact_us.png";
+import { Form, FormInputField } from "../../Form/Form";
 
 const Contact = () => {
   const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
@@ -31,27 +32,45 @@ const Contact = () => {
   };
 
   return (
-    <div>
-      <div>
-        {/* <Image src={} alt="contact us" /> */}
-        <img src={contact.src} alt="contact us" />
+    <section className="contact-section">
+      <h1>
+        Contact<span> Us</span>
+      </h1>
+      <div className="contact">
+        <div className="contact__image">
+          <img src={contact.src} alt="contact us" />
+        </div>
+        <div
+          data-testid="contact-header"
+          data-aos="zoom-in-up"
+          className="contact__container"
+        >
+          <div className="contact__container__form">
+            <Form onSubmit={sendEmail}>
+              <FormInputField name="name" type="text">
+                Name
+              </FormInputField>
+              <FormInputField name="email" type="text" required>
+                Email
+              </FormInputField>
+              <FormInputField name="message" row={6} type="textarea" required>
+                Message
+              </FormInputField>
+              <FormInputField type="submit" value={"Send"} />
+            </Form>
+          </div>
+
+          {/* <form className="contact-form" onSubmit={sendEmail}>
+            <label>Name</label>
+            <input type="text" name="name" required />
+            <label>Email</label>
+            <input type="email" name="email" required /> <label>Message</label>
+            <textarea name="message" cols={10} rows={8} required />
+            <input type="submit" value="Send" className="button" />
+          </form> */}
+        </div>
       </div>
-      <div
-        data-testid="contact-header"
-        data-aos="zoom-in-up"
-        className="contact-container"
-      >
-        <h2>Contact Us</h2>
-        <form className="contact-form" onSubmit={sendEmail}>
-          <label>Name</label>
-          <input type="text" name="name" required />
-          <label>Email</label>
-          <input type="email" name="email" required /> <label>Message</label>
-          <textarea name="message" cols={10} rows={8} required />
-          <input type="submit" value="Send" className="button" />
-        </form>
-      </div>
-    </div>
+    </section>
   );
 };
 
