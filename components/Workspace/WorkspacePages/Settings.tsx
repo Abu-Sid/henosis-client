d1fee29ade57d72871320e40463c0b7ec3c637ce
Abu Siddique <asrafeee@yahoo.com>
2021-08-31 23:46:13 -0400

    workespace ui updated

--- components/Workspace/WorkspacePages/Settings.tsx
+++ components/Workspace/WorkspacePages/Settings.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import coming from "../../../public/images/coming.png";
 
 const Settings = () => {
   return (
-    <div>
-      <h1>This is settings page</h1>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>This is Setting Page and Its in Under Construction </h1>
+      <img width={800} src={coming.src} alt="" />
     </div>
   );
 };
